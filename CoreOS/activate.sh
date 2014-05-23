b3b593255832c865a9273d594996dfa4e91a2f8e
Erwin Embsen <eembsen@xebia.com>
2014-05-23 16:49:50 +0000

    Added CoreOS customization to give docker a socket

---
+++ CoreOS/activate.sh
@@ -0,0 +1,6 @@
+cp docker-tcp.socket /etc/systemd/system/docker-tcp.socket
+systemctl enable docker-tcp.socket
+systemctl stop docker
+systemctl start docker-tcp.socket
+systemctl start docker
+docker -H tcp://127.0.0.1:4243 ps
